05885d1d91f841b02fafef3ab8b56d45015b1ebf
Nate G <ignitris@gmail.com>
2026-02-25 20:58:29 -0500

    Delete api directory

--- api/referral.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-    const { code } = req.query;
-    if (!code) return res.redirect(302, '/');
-    return res.redirect(302, `/?ref=${encodeURIComponent(code)}`);
-}
